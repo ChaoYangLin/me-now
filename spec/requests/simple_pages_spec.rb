cad6d2a1ee80f9ee843f038bc6b714cece660078
Yang <clinmyapp@gmail.com>
2014-02-04 12:22:21 -0500

    Create simple pages

--- spec/requests/simple_pages_spec.rb
+++ spec/requests/simple_pages_spec.rb
@@ -7,12 +7,22 @@ describe "SimplePages" do
       visit '/simple_pages/home'
       expect(page).to have_content('Web App')
     end
+    
+    it "should have the title 'Home'" do
+      visit '/simple_pages/home'
+      expect(page).to have_title('Home')
+    end
   end
   
     describe "Help page" do
     it "should have the content 'Help'" do
       visit '/simple_pages/help'
       expect(page).to have_content('Help')
+    end
+    
+    it "should have the title 'Help'" do
+      visit '/simple_pages/help'
+      expect(page).to have_title('Help')
     end
   end
   
@@ -21,6 +31,12 @@ describe "SimplePages" do
       visit '/simple_pages/about'
       expect(page).to have_content('About me-now')
     end
+    
+    it "should have the title 'About me-now'" do
+      visit '/simple_pages/about'
+      expect(page).to have_title('About me-now')
+    end
   end
+
   
 end
